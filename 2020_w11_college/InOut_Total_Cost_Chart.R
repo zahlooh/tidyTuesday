8e4b9c886e3f7ed545232cfae6d37ddf2e2a2bcc
zahlooh <dahbka@gmail.com>
2020-03-11 21:14:44 -0400

    initial version

---
+++ 2020_w11_college/InOut_Total_Cost_Chart.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+
+if (!exists("tuition_cost")) {
+  tuition_cost <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/tuition_cost.csv')
+}
+
+colors <- c("#F8F2D2", "#819975", "#DE6D43", "#424864")
+
+tuition_cost %>%
+  filter(!is.na(state)) %>%
+  group_by(state) %>%
+  summarise(outof = mean(out_of_state_tuition),
+            inst = mean(in_state_tuition)) %>%
+  ungroup() %>%
+  mutate(state = fct_reorder(state, outof / inst)) %>%
+  pivot_longer(cols = c(outof, inst), 
+               names_to = "inout",
+               values_to = "cost") %>%
+  mutate(inout = ifelse(substr(as.character(inout), 1, 3) == "out", "out", "in"),
+         inout = factor(inout, levels = c("out", "in"))) %>%
+  ggplot(aes(x = state, y = cost, fill = inout)) +
+    geom_col(position = "fill", width = .8) +
+    scale_fill_manual(values = c("in" = colors[2], "out" = colors[3])) +
+    scale_y_continuous(labels = scales::percent) +
+    coord_flip() +
+    labs(title = "In- vs Out-of- state tuition cost", caption = "Source of data: tuitiontracker.org") +
+    theme(axis.title = element_blank(),
+          legend.title = element_blank())
